57533cd187714c82360f457ec11dc9700e3c89f2
Zack Lee <cuinjune@gmail.com>
2020-12-08 00:54:23 -0500

    test

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
+import sys
 import os
 import json
 from collections import Counter
-from string import punctuation
+import spacy
 from flask import Flask, request
 from gevent.pywsgi import WSGIServer
 from decouple import config
 from pixabay import Image, Video
-import spacy
 
+nlp = spacy.load("en_core_web_md")
+
+def get_keywords(text):
+    doc = nlp(text)
+    entities = [e.text for e in list(doc.ents) if e.label_ in ["PERSON", "NORP", "FAC", "ORG", "GPE", "LOC", "PRODUCT", "EVENT"]]
+    words = [w.text for w in list(doc) if w.is_alpha and not w.is_stop and not w.is_punct and w.tag_ in ["NN", "NNS", "NNP", "NNPS", "VB", "VBD", "VBG", "VBP", "VBZ", "AFX", "JJ"]]
+    return entities + words
+
+# keywords test mode (by passing a sentence as arguments)
+if len(sys.argv) > 1:
+    # get text from system arguments
+    sentence = " ".join(sys.argv[1:])
+    keywords = get_keywords(sentence)
+    keywords = [k[0] for k in Counter(keywords).most_common(5)] # keywords to search in order
+    print("keywords", keywords)
+    exit()
+    
 app = Flask(__name__)
 PORT = int(os.environ.get("PORT", 3000))
 PIXABAY_API_AUTH_KEY = os.environ.get(
@@ -15,19 +32,6 @@ PIXABAY_API_AUTH_KEY = os.environ.get(
 
 video = Video(PIXABAY_API_AUTH_KEY)
 image = Image(PIXABAY_API_AUTH_KEY)
-nlp = spacy.load("en_core_web_md")
-
-def get_keywords(text):
-    result = []
-    pos_tag = ["PROPN", "ADJ", "NOUN"]
-    doc = nlp(text.lower())
-    for token in doc:
-        if token.text in nlp.Defaults.stop_words or token.text in punctuation:
-            continue
-        if token.pos_ in pos_tag:
-            result.append(token.text)
-    result = [w[0] for w in Counter(result).most_common(5)]
-    return result
 
 def get_updated_data(data):
     # avoid using repeated or cors blocked contents
@@ -36,10 +40,11 @@ def get_updated_data(data):
     for sentence in data["sentences"]:
         # per sentence
         min_duration = sentence["time"] / 1000
-        max_duration = min(min_duration + 30, 60)
+        max_duration = 60
         keywords = get_keywords(sentence["value"]) or [sentence["value"]]
+        doc_keywords = nlp(" ".join(keywords)) # used for similarity
+        keywords = [k[0] for k in Counter(keywords).most_common(5)] # keywords to search in order
         print("sentence:", sentence["value"], "\nkeywords", keywords)
-        doc_keywords = nlp(" ".join(keywords))
         id_ = 0
         url = ""
         for keyword in keywords:
@@ -48,11 +53,11 @@ def get_updated_data(data):
             max_hit = dict()
             for hit in video.search(q=keyword, per_page=200)["hits"]:
                 if  min_duration <= hit["duration"] <= max_duration and hit["id"] not in video_ids:
-                    similarity = doc_keywords.similarity(nlp(hit["tags"]))
+                    similarity = doc_keywords.similarity(nlp(hit["tags"].replace(",", "")))
                     if similarity > max_similarity:
                         max_similarity = similarity
                         max_hit = hit
-            if max_similarity and max_hit:
+            if max_similarity > 0.5 and max_hit:
                 id_ = max_hit["id"]
                 video_ids.add(id_);
                 url = max_hit["videos"]["medium"]["url"]
@@ -64,11 +69,11 @@ def get_updated_data(data):
                 max_hit = dict()
                 for hit in image.search(q=keyword, per_page=200)["hits"]:
                     if hit["id"] not in image_ids:
-                        similarity = doc_keywords.similarity(nlp(hit["tags"]))
+                        similarity = doc_keywords.similarity(nlp(hit["tags"].replace(",", "")))
                         if similarity > max_similarity:
                             max_similarity = similarity
                             max_hit = hit
-                if max_similarity and max_hit:
+                if max_similarity > 0.5 and max_hit:
                     id_ = max_hit["id"]
                     image_ids.add(id_);
                     url = max_hit["webformatURL"]
